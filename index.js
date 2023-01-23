103a1ca9e197aeaf183ffe1e7eb1446aca958192
NexyusNex <shootdietale@gmail.com>
2023-01-23 19:43:48 +0100

    Changed code to allow multiple menus without conflict

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const selected = document.querySelector(".selected");
-selected.addEventListener("click", function (e) {
-  e.preventDefault();
-  const menuList = document.querySelector(".menu-list");
-  if (menuList.style.visibility != "visible") {
-    menuList.style.visibility = "visible";
-    menuList.classList.add("animated");
-  } else {
-    menuList.style.visibility = "hidden";
-    menuList.classList.remove("animated");
-  }
-});
+const dropMenu = document.querySelectorAll(".drop-down-menu");
+dropMenu.forEach((menu) => {
+  const selected = menu.querySelector(".selected");
+  const menuList = menu.querySelector(".menu-list");
+  selected.addEventListener("click", function (e) {
+    e.preventDefault();
+    const icon = selected.querySelector(".selected-icon");
+    if (menuList.style.visibility != "visible") {
+      menuList.style.visibility = "visible";
+      menuList.classList.add("animated");
+      icon.style.transform = "rotate(180deg)";
+    } else {
+      menuList.style.visibility = "hidden";
+      menuList.classList.remove("animated");
+      icon.style.transform = "rotate(0)";
+    }
+  });
 
-const options = document.querySelectorAll(".option");
-options.forEach((option) => {
-  option.addEventListener("click", function (e) {
-    e.stopPropagation();
-    document.querySelector(".selected-text").textContent = option.textContent;
+  const options = menuList.querySelectorAll(".option");
+  options.forEach((option) => {
+    option.addEventListener("click", function (e) {
+      e.stopPropagation();
+      menu.querySelector(".selected-text").textContent = option.textContent;
+    });
   });
 });
