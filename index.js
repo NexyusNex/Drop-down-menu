e3c9d3f62301c1cd3567c232f3ae828705c1262a
NexyusNex <shootdietale@gmail.com>
2023-01-23 19:50:33 +0100

    Made it a function

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const dropMenu = document.querySelectorAll(".drop-down-menu");
-dropMenu.forEach((menu) => {
-  const selected = menu.querySelector(".selected");
-  const menuList = menu.querySelector(".menu-list");
-  const icon = selected.querySelector(".selected-icon");
-  selected.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (menuList.style.visibility != "visible") {
-      menuList.style.visibility = "visible";
-      menuList.classList.add("animated");
-      icon.style.transform = "rotate(180deg)";
-    } else {
-      menuList.style.visibility = "hidden";
-      menuList.classList.remove("animated");
-      icon.style.transform = "rotate(0)";
-    }
-  });
+function MenuFunctionality() {
+  const dropMenu = document.querySelectorAll(".drop-down-menu");
+  dropMenu.forEach((menu) => {
+    const selected = menu.querySelector(".selected");
+    const menuList = menu.querySelector(".menu-list");
+    const icon = selected.querySelector(".selected-icon");
+    selected.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (menuList.style.visibility != "visible") {
+        menuList.style.visibility = "visible";
+        menuList.classList.add("animated");
+        icon.style.transform = "rotate(180deg)";
+      } else {
+        menuList.style.visibility = "hidden";
+        menuList.classList.remove("animated");
+        icon.style.transform = "rotate(0)";
+      }
+    });
 
-  const options = menuList.querySelectorAll(".option");
-  options.forEach((option) => {
-    option.addEventListener("click", function (e) {
-      e.stopPropagation();
-      menu.querySelector(".selected-text").textContent = option.textContent;
-      menuList.style.visibility = "hidden";
-      menuList.classList.remove("animated");
-      icon.style.transform = "rotate(0)";
+    const options = menuList.querySelectorAll(".option");
+    options.forEach((option) => {
+      option.addEventListener("click", function (e) {
+        e.stopPropagation();
+        menu.querySelector(".selected-text").textContent = option.textContent;
+        menuList.style.visibility = "hidden";
+        menuList.classList.remove("animated");
+        icon.style.transform = "rotate(0)";
+      });
     });
   });
-});
+}
+
+MenuFunctionality();
